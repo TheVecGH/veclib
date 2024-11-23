9258e6e8336c94b6a6c6946264f4eeef6fe4b514
Niels Slotboom <slotboom.n@gmail.com>
2024-11-23 20:10:53 +0100

    Implemented Christoffel symbols, testing still required

--- veclib/veclib/spacetime.py
+++ veclib/veclib/spacetime.py
@@ -1,9 +1,11 @@
 import sympy as sp
+from .tensor import Tensor 
 
 dim = None
 metric = None
 metric_inv = None
 coords = None
+christoffel = None
 
 def set_metric_and_coordinates(g, coords_input):
     """
@@ -14,15 +16,28 @@ def set_metric_and_coordinates(g, coords_input):
         coords (tuple): A tuple of sympy symbols representing the coordinates.
     """
 
-    global metric, metric_inv, coords, dim  # Use the global variables
+    global metric, metric_inv, coords, dim, christoffel  # Use the global variables
 
-    g_components = sp.MutableDenseNDimArray(g.get_components())
+    g_components = sp.MutableDenseNDimArray(g.components)
     if g_components.rank() != 2:
         raise ValueError(
-                f"Metric tensor must have rank 2, not {g_components.rank()}"
+                f"Metric tensor must have rank 2, not {g_components.rank}"
             )
 
     metric = sp.Matrix(g_components)
     metric_inv = metric.inv()
+    metric = sp.MutableDenseNDimArray(metric)
+    metric_inv = sp.MutableDenseNDimArray(metric_inv)
     coords = coords_input
     dim = len(coords)
+    christoffel = None #clearing christoffel symbols
+
+def ensure_christoffel():
+
+    global christoffel
+
+    if metric == None:
+        raise ValueError("Metric tensor not set.")
+    if christoffel == None:
+        dg = Tensor("g", metric, [-1,-1]).partial_gradient()
+        christoffel = Tensor(r"Γ", (sp.Rational(1,2) * (dg.swap_indices(0,2) + dg.swap_indices(0,1) - dg)).raise_index(0))
